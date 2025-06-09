aed0fa9fafedd8078fe37a7bc50d15c1fdb7fade
Rafael Fonseca <rafael.abreu.fonseca@gmail.com>
2025-06-09 17:46:16 +0200

    Scaffolding resource companies/partners

--- src/companies/companies.module.ts
+++ src/companies/companies.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CompaniesService } from './companies.service';
 import { CompaniesController } from './companies.controller';
+import { PartnersModule } from './partners/partners.module';
 
 @Module({
   controllers: [CompaniesController],
   providers: [CompaniesService],
+  imports: [PartnersModule],
 })
 export class CompaniesModule {}
