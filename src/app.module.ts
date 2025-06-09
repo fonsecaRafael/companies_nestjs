12dd882ed0ffa54751d3299076de94f1bbba77fb
Rafael Fonseca <rafael.abreu.fonseca@gmail.com>
2025-06-09 17:44:23 +0200

    Scaffolding resource addresses

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CompaniesModule } from './companies/companies.module';
+import { AddressesModule } from './addresses/addresses.module';
 
 @Module({
-  imports: [CompaniesModule],
+  imports: [CompaniesModule, AddressesModule],
   controllers: [AppController],
   providers: [AppService],
 })
