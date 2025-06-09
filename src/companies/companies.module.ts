2b19905d1b0634c53f83b82327fef6c568debb2b
Rafael Fonseca <rafael.abreu.fonseca@gmail.com>
2025-06-09 17:47:35 +0200

    Scaffolding resource companies/revenues

--- src/companies/companies.module.ts
+++ src/companies/companies.module.ts
@@ -3,10 +3,11 @@ import { CompaniesService } from './companies.service';
 import { CompaniesController } from './companies.controller';
 import { PartnersModule } from './partners/partners.module';
 import { CnaesModule } from './cnaes/cnaes.module';
+import { RevenuesModule } from './revenues/revenues.module';
 
 @Module({
   controllers: [CompaniesController],
   providers: [CompaniesService],
-  imports: [PartnersModule, CnaesModule],
+  imports: [PartnersModule, CnaesModule, RevenuesModule],
 })
 export class CompaniesModule {}
