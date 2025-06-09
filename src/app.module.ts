cebea56cade667a68a8a96e168f6255247f628f7
Rafael Fonseca <rafael.abreu.fonseca@gmail.com>
2025-06-09 17:45:04 +0200

    Scaffolding resource contacts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CompaniesModule } from './companies/companies.module';
 import { AddressesModule } from './addresses/addresses.module';
+import { ContactsModule } from './contacts/contacts.module';
 
 @Module({
-  imports: [CompaniesModule, AddressesModule],
+  imports: [CompaniesModule, AddressesModule, ContactsModule],
   controllers: [AppController],
   providers: [AppService],
 })
