3dab32c9d0fcc5d0a769c6a28308a4238082019c
Rafael Fonseca <rafael.abreu.fonseca@gmail.com>
2025-06-11 21:31:09 +0200

    Adds Config to TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,33 @@ import { AppService } from './app.service';
 import { CompaniesModule } from './companies/companies.module';
 import { AddressesModule } from './addresses/addresses.module';
 import { ContactsModule } from './contacts/contacts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [CompaniesModule, AddressesModule, ContactsModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get<string>('DATABASE_USERNAME'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: false,
+        logging: true,
+        migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+        migrationsRun: false,
+      }),
+      inject: [ConfigService],
+    }),
+    CompaniesModule,
+    AddressesModule,
+    ContactsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ typeorm.config.ts
@@ -0,0 +1,13 @@
+module.exports = {
+  type: 'postgres',
+  host: process.env.DATABASE_HOST,
+  port: parseInt(process.env.DATABASE_PORT || '0000'),
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  migrations: ['dist/migrations/**/*{.ts,.js}'],
+  cli: {
+    migrationsDir: 'src/migrations',
+  },
+};
