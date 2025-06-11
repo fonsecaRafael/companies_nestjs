f121dbcc715c8277227bd8999b9c2f084dd78008
Rafael Fonseca <rafael.abreu.fonseca@gmail.com>
2025-06-11 21:38:10 +0200

    Minor fixes just to compile after insertion of company entity

--- src/companies/companies.module.ts
+++ src/companies/companies.module.ts
@@ -5,10 +5,12 @@ import { PartnersModule } from './partners/partners.module';
 import { CnaesModule } from './cnaes/cnaes.module';
 import { RevenuesModule } from './revenues/revenues.module';
 import { ProductsModule } from './products/products.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Company } from './entities/company.entity';
 
 @Module({
   controllers: [CompaniesController],
   providers: [CompaniesService],
-  imports: [PartnersModule, CnaesModule, RevenuesModule, ProductsModule],
+  imports: [PartnersModule, CnaesModule, RevenuesModule, ProductsModule, TypeOrmModule.forFeature([Company])],
 })
 export class CompaniesModule {}
