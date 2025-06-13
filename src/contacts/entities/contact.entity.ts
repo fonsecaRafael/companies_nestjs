9d109c06c117147b53dad003ed414e795ab0e47b
Rafael Fonseca <rafael.abreu.fonseca@gmail.com>
2025-06-13 12:51:28 +0200

    Adds Contact Entity

--- src/contacts/entities/contact.entity.ts
+++ src/contacts/entities/contact.entity.ts
@@ -1,1 +1,23 @@
-export class Contact {}
+import { Company } from 'src/companies/entities/company.entity';
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity()
+export class Contact {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  phone: string;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  updateAt: Date;
+
+  // Relationship
+  @ManyToOne(() => Company, (company) => company.contacts)
+  @JoinColumn({ name: 'company_id' })
+  company: Company;
+}
