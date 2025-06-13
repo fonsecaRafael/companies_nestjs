24a2551075dfa1b87454fa6204bc512b7dd0db33
Rafael Fonseca <rafael.abreu.fonseca@gmail.com>
2025-06-13 14:46:01 +0200

    Adds revenue entity

--- src/companies/revenues/entities/revenue.entity.ts
+++ src/companies/revenues/entities/revenue.entity.ts
@@ -1,1 +1,21 @@
-export class Revenue {}
+import { Company } from 'src/companies/entities/company.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Revenue {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('int')
+  year: number;
+
+  @Column('int')
+  month: number;
+
+  @Column('decimal', { precision: 15, scale: 2 })
+  value: number;
+
+  // Relationship
+  @ManyToOne(() => Company, (company) => company.revenues)
+  company: Company;
+}
