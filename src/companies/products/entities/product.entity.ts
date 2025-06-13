528013395b9b07a1055faf87e4057d095aa7b86d
Rafael Fonseca <rafael.abreu.fonseca@gmail.com>
2025-06-13 15:07:36 +0200

    Adds product entity and necessary Enum

--- src/companies/products/entities/product.entity.ts
+++ src/companies/products/entities/product.entity.ts
@@ -1,1 +1,25 @@
-export class Product {}
+import { Company } from 'src/companies/entities/company.entity';
+import { ProductType } from 'src/shared/enums/product-type.enum';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'enum', enum: ProductType })
+  type: ProductType;
+
+  @Column({ type: 'varchar', length: 20 })
+  code: string;
+
+  @Column({ type: 'varchar', length: 200 })
+  description: string;
+
+  @Column('decimal', { precision: 11, scale: 2 })
+  price: number;
+
+  // Relationship
+  @ManyToOne(() => Company, (company) => company.products)
+  company: Company;
+}
---
+++ src/shared/enums/product-type.enum.ts
@@ -0,0 +1,4 @@
+export enum ProductType {
+  PRODUCT = 'Produto',
+  SERVICE = 'Serviço',
+}
