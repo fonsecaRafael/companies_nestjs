e19076d19b6c4d72405cf42d6e3a8bfa356c3ed0
Rafael Fonseca <rafael.abreu.fonseca@gmail.com>
2025-06-13 14:22:11 +0200

    Adds Cnae Entity

--- src/companies/cnaes/entities/cnae.entity.ts
+++ src/companies/cnaes/entities/cnae.entity.ts
@@ -1,1 +1,27 @@
-export class Cnae {}
+import { Company } from 'src/companies/entities/company.entity';
+import { Column, Entity, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+
+@Entity()
+export class Cnae {
+  /********************************************************************************
+   ** This entity should have  your values to code and description obtained from **
+   ** an IBGE table, and made a relationship 1 Cnae to N companies in order to   **
+   ** normalization of DB but this table is huge and make the project to complex **
+   ** So lets make a simplification here                                         **
+   ********************************************************************************/
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  code: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  main: boolean;
+
+  // Relationship
+  @ManyToOne(() => Company, (company) => company.cnaes)
+  company: Company;
+}
