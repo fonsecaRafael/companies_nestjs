2b54cb870ab0278b57a81d7a39173acb35a177e6
Rafael Fonseca <rafael.abreu.fonseca@gmail.com>
2025-06-15 08:56:08 +0200

    Fix import in company entity

--- src/companies/companies.module.ts
+++ src/companies/companies.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { CompaniesService } from './companies.service';
 import { CompaniesController } from './companies.controller';
-import { PartnersModule } from './partners/partners.module';
+import { PartnersModule } from '../partners/partners.module';
 import { CnaesModule } from './cnaes/cnaes.module';
 import { RevenuesModule } from './revenues/revenues.module';
 import { ProductsModule } from './products/products.module';
