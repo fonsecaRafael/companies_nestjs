09bc4a017690ca6246332ac9bba9c32443308724
Rafael Fonseca <rafael.abreu.fonseca@gmail.com>
2025-06-18 21:46:13 +0200

    Makes cnae controller tests works

--- src/companies/cnaes/cnaes.controller.ts
+++ src/companies/cnaes/cnaes.controller.ts
@@ -2,13 +2,14 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { CnaesService } from './cnaes.service';
 import { CreateCnaeDto } from './dto/create-cnae.dto';
 import { UpdateCnaeDto } from './dto/update-cnae.dto';
+import { Cnae } from './entities/cnae.entity';
 
 @Controller('cnaes')
 export class CnaesController {
   constructor(private readonly cnaesService: CnaesService) {}
 
   @Post()
-  create(@Body() createCnaeDto: CreateCnaeDto) {
+  create(@Body() createCnaeDto: CreateCnaeDto): Promise<Cnae> {
     return this.cnaesService.create(createCnaeDto);
   }
 
@@ -18,17 +19,17 @@ export class CnaesController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.cnaesService.findOne(+id);
+  findOne(@Param('id') id: number) {
+    return this.cnaesService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCnaeDto: UpdateCnaeDto) {
-    return this.cnaesService.update(+id, updateCnaeDto);
+  update(@Param('id') id: number, @Body() updateCnaeDto: UpdateCnaeDto) {
+    return this.cnaesService.update(id, updateCnaeDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.cnaesService.remove(+id);
+  remove(@Param('id') id: number) {
+    return this.cnaesService.softDelete(id);
   }
 }
