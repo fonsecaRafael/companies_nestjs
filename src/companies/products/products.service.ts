ac8184ae9b4aec5e3a3b60ea9af70dc8560daac9
Rafael Fonseca <rafael.abreu.fonseca@gmail.com>
2025-06-19 03:13:20 +0200

    Adds products service

--- src/companies/products/products.service.ts
+++ src/companies/products/products.service.ts
@@ -1,26 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  constructor(
+    @InjectRepository(Product)
+    private productRepository: Repository<Product>,
+  ) {}
+
+  async create(createProductDto: CreateProductDto) {
+    const newproduct = this.productRepository.create(createProductDto);
+    return this.productRepository.save(newproduct);
   }
 
-  findAll() {
-    return `This action returns all products`;
+  async findAll(): Promise<Product[]> {
+    return this.productRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: number): Promise<Product> {
+    const product = await this.productRepository.findOne({
+      where: { id },
+    });
+    if (!product) throw new NotFoundException('CNAE não encontrado');
+    return product;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: number, updateCnaeDto: UpdateProductDto): Promise<Product> {
+    const product = await this.productRepository.findOne({
+      where: { id },
+    });
+    if (!product) {
+      throw new NotFoundException('Produto não encontrado');
+    }
+    this.productRepository.merge(product, updateCnaeDto);
+    return this.productRepository.save(product);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async softDelete(id: number): Promise<void> {
+    const result = await this.productRepository.softDelete({ id });
+    if (result.affected === 0) {
+      throw new NotFoundException('Cnae não encontrado.');
+    }
   }
 }
