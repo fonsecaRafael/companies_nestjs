3c74679931f05b5dafd94bfb13d47a2249409549
Rafael Fonseca <rafael.abreu.fonseca@gmail.com>
2025-06-19 02:58:23 +0200

    Makes CNAEs integration test pass

--- src/companies/cnaes/dto/create-cnae.dto.ts
+++ src/companies/cnaes/dto/create-cnae.dto.ts
@@ -1,1 +1,25 @@
-export class CreateCnaeDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsBoolean, IsString } from 'class-validator';
+
+export class CreateCnaeDto {
+  @ApiProperty({
+    example: '2599-3/02',
+    description: 'Código informado pelo IBGE. Tamanho máximo 10.',
+  })
+  @IsString()
+  code: string;
+
+  @ApiProperty({
+    example: 'FERRO E AÇO CORTE E DOBRA (NÃO ASSOCIADO AO COMÉRCIO)',
+    description: 'Descrição informada pelo IBGE.',
+  })
+  @IsString()
+  description: string;
+
+  @ApiProperty({
+    example: 'true',
+    description: 'Informa se esta é a atividade principal executada pela empresa ou não.',
+  })
+  @IsBoolean()
+  main: boolean;
+}
--- src/companies/cnaes/dto/update-cnae.dto.ts
+++ src/companies/cnaes/dto/update-cnae.dto.ts
@@ -1,4 +1,29 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiPropertyOptional, PartialType } from '@nestjs/swagger';
 import { CreateCnaeDto } from './create-cnae.dto';
+import { IsBoolean, IsOptional, IsString } from 'class-validator';
 
-export class UpdateCnaeDto extends PartialType(CreateCnaeDto) {}
+export class UpdateCnaeDto extends PartialType(CreateCnaeDto) {
+  @ApiPropertyOptional({
+    example: '2599-3/02',
+    description: 'Código informado pelo IBGE. Tamanho máximo 10.',
+  })
+  @IsString()
+  @IsOptional()
+  code: string;
+
+  @ApiPropertyOptional({
+    example: 'FERRO E AÇO CORTE E DOBRA (NÃO ASSOCIADO AO COMÉRCIO)',
+    description: 'Descrição informada pelo IBGE.',
+  })
+  @IsString()
+  @IsOptional()
+  description: string;
+
+  @ApiPropertyOptional({
+    example: 'true',
+    description: 'Informa se esta é a atividade principal executada pela empresa ou não.',
+  })
+  @IsBoolean()
+  @IsOptional()
+  main: boolean;
+}
