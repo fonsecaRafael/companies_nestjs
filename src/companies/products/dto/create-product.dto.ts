32af1d9ea32c5b22545d63928503bf096525de56
Rafael Fonseca <rafael.abreu.fonseca@gmail.com>
2025-06-19 03:38:33 +0200

    Adds DTO for products

--- src/companies/products/dto/create-product.dto.ts
+++ src/companies/products/dto/create-product.dto.ts
@@ -1,1 +1,30 @@
-export class CreateProductDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEnum, IsNumber, IsString } from 'class-validator';
+import { ProductType } from 'src/shared/enums';
+
+export class CreateProductDto {
+  @ApiProperty({ enum: ProductType, example: ProductType.PRODUCT })
+  @IsEnum(ProductType)
+  type: ProductType;
+
+  @ApiProperty({
+    example: 'JBTY9856KL3',
+    description: 'Código que identifica o produto. Tamanho máximo 20 caracteres.',
+  })
+  @IsString()
+  code: string;
+
+  @ApiProperty({
+    example: 'Esta é a caixa de som mais potente do Brasil!',
+    description: 'Descrição do produto. Tamanho máximo 200 caracteres.',
+  })
+  @IsString()
+  description: string;
+
+  @ApiProperty({
+    example: 200.25,
+    description: 'Valor com até duas casas decimais que representa o preço do produto em Reais.'
+  })
+  @IsNumber()
+  price: number;
+}
--- src/companies/products/dto/update-product.dto.ts
+++ src/companies/products/dto/update-product.dto.ts
@@ -1,4 +1,35 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiPropertyOptional, PartialType } from '@nestjs/swagger';
 import { CreateProductDto } from './create-product.dto';
+import { ProductType } from 'src/shared/enums';
+import { IsEnum, IsNumber, IsOptional, IsString } from 'class-validator';
 
-export class UpdateProductDto extends PartialType(CreateProductDto) {}
+export class UpdateProductDto extends PartialType(CreateProductDto) {
+  @ApiPropertyOptional({ enum: ProductType, example: ProductType.PRODUCT })
+  @IsEnum(ProductType)
+  @IsOptional()
+  type: ProductType;
+
+  @ApiPropertyOptional({
+    example: 'JBTY9856KL3',
+    description: 'Código que identifica o produto. Tamanho máximo 20 caracteres.',
+  })
+  @IsString()
+  @IsOptional()
+  code: string;
+
+  @ApiPropertyOptional({
+    example: 'Esta é a caixa de som mais potente do Brasil!',
+    description: 'Descrição do produto. Tamanho máximo 200 caracteres.',
+  })
+  @IsString()
+  @IsOptional()
+  description: string;
+
+  @ApiPropertyOptional({
+    example: 200.25,
+    description: 'Valor com até duas casas decimais que representa o preço do produto em Reais.',
+  })
+  @IsNumber()
+  @IsOptional()
+  price: number;
+}
