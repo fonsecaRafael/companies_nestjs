d54784085f1812f283f41188b0f85517856c2976
Rafael Fonseca <rafael.abreu.fonseca@gmail.com>
2025-06-24 19:33:15 +0200

    Adds revenues controller and service

--- src/companies/revenues/revenues.controller.ts
+++ src/companies/revenues/revenues.controller.ts
@@ -2,13 +2,14 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { RevenuesService } from './revenues.service';
 import { CreateRevenueDto } from './dto/create-revenue.dto';
 import { UpdateRevenueDto } from './dto/update-revenue.dto';
+import { Revenue } from './entities/revenue.entity';
 
 @Controller('revenues')
 export class RevenuesController {
   constructor(private readonly revenuesService: RevenuesService) {}
 
   @Post()
-  create(@Body() createRevenueDto: CreateRevenueDto) {
+  create(@Body() createRevenueDto: CreateRevenueDto): Promise<Revenue> {
     return this.revenuesService.create(createRevenueDto);
   }
 
@@ -18,17 +19,17 @@ export class RevenuesController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.revenuesService.findOne(+id);
+  findOne(@Param('id') id: number) {
+    return this.revenuesService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateRevenueDto: UpdateRevenueDto) {
-    return this.revenuesService.update(+id, updateRevenueDto);
+  update(@Param('id') id: number, @Body() updateRevenueDto: UpdateRevenueDto) {
+    return this.revenuesService.update(id, updateRevenueDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.revenuesService.remove(+id);
+  remove(@Param('id') id: number) {
+    return this.revenuesService.softDelete(id);
   }
 }
--- src/companies/revenues/revenues.service.ts
+++ src/companies/revenues/revenues.service.ts
@@ -1,26 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateRevenueDto } from './dto/create-revenue.dto';
 import { UpdateRevenueDto } from './dto/update-revenue.dto';
+import { Revenue } from './entities/revenue.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class RevenuesService {
-  create(createRevenueDto: CreateRevenueDto) {
-    return 'This action adds a new revenue';
+  constructor(
+    @InjectRepository(Revenue)
+    private revenueRepository: Repository<Revenue>,
+  ) {}
+
+  async create(createRevenueDto: CreateRevenueDto) {
+    const newRevenue = this.revenueRepository.create(createRevenueDto);
+    return this.revenueRepository.save(newRevenue);
   }
 
-  findAll() {
-    return `This action returns all revenues`;
+  async findAll(): Promise<Revenue[]> {
+    return this.revenueRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} revenue`;
+  async findOne(id: number): Promise<Revenue> {
+    const revenue = await this.revenueRepository.findOne({
+      where: { id },
+    });
+    if (!revenue) throw new NotFoundException('Faturamento não encontrado');
+    return revenue;
   }
 
-  update(id: number, updateRevenueDto: UpdateRevenueDto) {
-    return `This action updates a #${id} revenue`;
+  async update(id: number, updateRevenueDto: UpdateRevenueDto): Promise<Revenue> {
+    const revenue = await this.revenueRepository.findOne({
+      where: { id },
+    });
+    if (!revenue) {
+      throw new NotFoundException('Faturamento não encontrado');
+    }
+    this.revenueRepository.merge(revenue, updateRevenueDto);
+    return this.revenueRepository.save(revenue);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} revenue`;
+  async softDelete(id: number): Promise<void> {
+    const result = await this.revenueRepository.softDelete({ id });
+    if (result.affected === 0) {
+      throw new NotFoundException('Faturamento não encontrado.');
+    }
   }
 }
