e0d9886780ff35986c4b9269eaa0f815feb6cd19
Rafael Fonseca <rafael.abreu.fonseca@gmail.com>
2025-06-24 19:31:56 +0200

    Converts Revenues module in a Type Orm Module

--- src/companies/revenues/revenues.module.ts
+++ src/companies/revenues/revenues.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { RevenuesService } from './revenues.service';
 import { RevenuesController } from './revenues.controller';
+import { Revenue } from './entities/revenue.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   controllers: [RevenuesController],
   providers: [RevenuesService],
+  imports: [TypeOrmModule.forFeature([Revenue])],
 })
 export class RevenuesModule {}
