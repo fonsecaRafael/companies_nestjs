e08a5c433530118828f87b583ef5e6379ed6cd37
Rafael Fonseca <rafael.abreu.fonseca@gmail.com>
2025-06-24 19:32:40 +0200

    Adds revenue DTO

--- src/companies/revenues/dto/create-revenue.dto.ts
+++ src/companies/revenues/dto/create-revenue.dto.ts
@@ -1,1 +1,25 @@
-export class CreateRevenueDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber } from 'class-validator';
+
+export class CreateRevenueDto {
+  @ApiProperty({
+    example: '2025',
+    description: '4 dígitos que representam o ano em questão.',
+  })
+  @IsNumber()
+  year: number;
+
+  @ApiProperty({
+    example: '02',
+    description: 'Número entre 01 e 12 que representa o mês do ano.',
+  })
+  @IsNumber()
+  month: number;
+
+  @ApiProperty({
+    example: '1500.25',
+    description: 'Valor com até duas casas decimais que representam o valor faturado naquele mês pela empresa.',
+  })
+  @IsNumber()
+  value: number;
+}
--- src/companies/revenues/dto/update-revenue.dto.ts
+++ src/companies/revenues/dto/update-revenue.dto.ts
@@ -1,4 +1,28 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateRevenueDto } from './create-revenue.dto';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { IsNumber, IsOptional } from 'class-validator';
 
-export class UpdateRevenueDto extends PartialType(CreateRevenueDto) {}
+export class UpdateRevenueDto {
+  @ApiPropertyOptional({
+    example: '2025',
+    description: '4 dígitos que representam o ano em questão.',
+  })
+  @IsNumber()
+  @IsOptional()
+  year: number;
+
+  @ApiPropertyOptional({
+    example: '02',
+    description: 'Número entre 01 e 12 que representa o mês do ano.',
+  })
+  @IsNumber()
+  @IsOptional()
+  month: number;
+
+  @ApiPropertyOptional({
+    example: '1500.25',
+    description: 'Valor com até duas casas decimais que representam o valor faturado naquele mês pela empresa.',
+  })
+  @IsNumber()
+  @IsOptional()
+  value: number;
+}
