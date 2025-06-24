0b8e18c909de8f72531312d9ab8676e9c752d912
Rafael Fonseca <rafael.abreu.fonseca@gmail.com>
2025-06-24 23:01:35 +0200

    Adds Contact Service

--- src/contacts/contacts.service.ts
+++ src/contacts/contacts.service.ts
@@ -1,26 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateContactDto } from './dto/create-contact.dto';
 import { UpdateContactDto } from './dto/update-contact.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Contact } from './entities/contact.entity';
+import { Company } from 'src/companies/entities/company.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ContactsService {
-  create(createContactDto: CreateContactDto) {
-    return 'This action adds a new contact';
+  constructor(
+    @InjectRepository(Contact)
+    private contactRepository: Repository<Contact>,
+    @InjectRepository(Company)
+    private companyRepository: Repository<Company>,
+  ) {}
+
+  async create(createContactDto: CreateContactDto): Promise<Contact> {
+    const company = await this.companyRepository.findOne({ where: { id: createContactDto.companyId } });
+    if (!company) {
+      throw new NotFoundException('Empresa não encontrada.');
+    }
+    const contact = this.contactRepository.create({ ...createContactDto, company });
+    return this.contactRepository.save(contact);
   }
 
-  findAll() {
-    return `This action returns all contacts`;
+  async findAll(): Promise<Contact[]> {
+    return this.contactRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} contact`;
+  async findOne(id: number): Promise<Contact> {
+    const contact = await this.contactRepository.findOne({ where: { id } });
+    if (!contact) {
+      throw new NotFoundException('Contato não encontrado.');
+    }
+    return contact;
   }
 
-  update(id: number, updateContactDto: UpdateContactDto) {
-    return `This action updates a #${id} contact`;
+  async update(id: number, updateContactDto: UpdateContactDto): Promise<Contact> {
+    const contact = await this.contactRepository.findOne({ where: { id } });
+    if (!contact) {
+      throw new NotFoundException('Contato não encontrado.');
+    }
+    this.contactRepository.merge(contact, updateContactDto);
+    return this.contactRepository.save(contact);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} contact`;
+  async softDelete(id: number): Promise<void> {
+    const result = await this.contactRepository.softDelete({ id });
+    if (result.affected === 0) {
+      throw new NotFoundException('Contato não encontrado.');
+    }
   }
 }
