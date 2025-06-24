b4928a83801edfa8f645d9e1a7f79b17c1d31c38
Rafael Fonseca <rafael.abreu.fonseca@gmail.com>
2025-06-24 22:14:53 +0200

    Converts contacts in a valid Type Orm Module

--- src/contacts/contacts.module.ts
+++ src/contacts/contacts.module.ts
@@ -3,9 +3,10 @@ import { ContactsService } from './contacts.service';
 import { ContactsController } from './contacts.controller';
 import { Contact } from './entities/contact.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Company } from 'src/companies/entities/company.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Contact])],
+  imports: [TypeOrmModule.forFeature([Contact, Company])],
   controllers: [ContactsController],
   providers: [ContactsService],
 })
