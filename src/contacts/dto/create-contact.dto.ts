0a9b6ed0bb9ecc071254ebdb9ea1b7319f24b5ac
Rafael Fonseca <rafael.abreu.fonseca@gmail.com>
2025-06-24 19:54:22 +0200

    Adds create contact DTO

--- src/contacts/dto/create-contact.dto.ts
+++ src/contacts/dto/create-contact.dto.ts
@@ -1,1 +1,11 @@
-export class CreateContactDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
+
+export class CreateContactDto {
+  @ApiProperty({
+    example: 'XX123459999',
+    description: 'Sequencia de números que representam um telefone brasileiro.',
+  })
+  @IsString()
+  phone: string;
+}
