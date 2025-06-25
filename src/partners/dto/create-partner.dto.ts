9344b23c6d1c1c67463302e1b17aa1b0efa056c4
Rafael Fonseca <rafael.abreu.fonseca@gmail.com>
2025-06-26 00:54:23 +0200

    adds create partner DTO

--- src/partners/dto/create-partner.dto.ts
+++ src/partners/dto/create-partner.dto.ts
@@ -1,1 +1,21 @@
-export class CreatePartnerDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsString } from 'class-validator';
+
+export class CreatePartnerDto {
+  @ApiProperty({
+    example: 'Rafael Abreu Fonseca',
+    description: 'Nome completo do sócio da empresa. Máximo 120 caracteres.',
+  })
+  @IsString()
+  name: string;
+
+  @ApiProperty({
+    example: '000.000.000-00',
+    description: 'Código que descreve um CPF.',
+  })
+  @IsString()
+  cpf: string;
+
+  @IsNumber()
+  companyId: number;
+}
