596a0339dfd4e78532557427e5f873234842203b
Rafael Fonseca <rafael.abreu.fonseca@gmail.com>
2025-06-26 00:46:26 +0200

    Converts partners in a valid Type Orm Module

--- src/partners/partners.module.ts
+++ src/partners/partners.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PartnersService } from './partners.service';
 import { PartnersController } from './partners.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Partner } from './entities/partner.entity';
+import { Company } from '../companies/entities/company.entity';
 
 @Module({
   controllers: [PartnersController],
   providers: [PartnersService],
+  imports: [TypeOrmModule.forFeature([Partner, Company])],
 })
 export class PartnersModule {}
