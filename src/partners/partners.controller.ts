87b05e879f428e0f1ebecb6f58ac8a88843ad766
Rafael Fonseca <rafael.abreu.fonseca@gmail.com>
2025-06-27 00:24:29 +0200

    Adds partners controller and service

--- src/partners/partners.controller.ts
+++ src/partners/partners.controller.ts
@@ -2,13 +2,14 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { PartnersService } from './partners.service';
 import { CreatePartnerDto } from './dto/create-partner.dto';
 import { UpdatePartnerDto } from './dto/update-partner.dto';
+import { Partner } from './entities/partner.entity';
 
 @Controller('partners')
 export class PartnersController {
   constructor(private readonly partnersService: PartnersService) {}
 
   @Post()
-  create(@Body() createPartnerDto: CreatePartnerDto) {
+  create(@Body() createPartnerDto: CreatePartnerDto): Promise<Partner> {
     return this.partnersService.create(createPartnerDto);
   }
 
@@ -18,17 +19,17 @@ export class PartnersController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.partnersService.findOne(+id);
+  findOne(@Param('id') id: number) {
+    return this.partnersService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePartnerDto: UpdatePartnerDto) {
-    return this.partnersService.update(+id, updatePartnerDto);
+  update(@Param('id') id: number, @Body() updatePartnerDto: UpdatePartnerDto) {
+    return this.partnersService.update(id, updatePartnerDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.partnersService.remove(+id);
+  remove(@Param('id') id: number) {
+    return this.partnersService.softDelete(id);
   }
 }
--- src/partners/partners.service.ts
+++ src/partners/partners.service.ts
@@ -1,26 +1,61 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreatePartnerDto } from './dto/create-partner.dto';
 import { UpdatePartnerDto } from './dto/update-partner.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Partner } from './entities/partner.entity';
+import { Repository } from 'typeorm';
+import { Company } from '../companies/entities/company.entity';
 
 @Injectable()
 export class PartnersService {
-  create(createPartnerDto: CreatePartnerDto) {
-    return 'This action adds a new partner';
+  constructor(
+    @InjectRepository(Partner)
+    private partnerRepository: Repository<Partner>,
+    @InjectRepository(Company)
+    private companyRepository: Repository<Company>,
+  ) {}
+
+  async create(createPartnerDto: CreatePartnerDto): Promise<Partner> {
+    const company = await this.companyRepository.findOne({
+      where: { id: createPartnerDto.companyId },
+    });
+
+    if (!company) {
+      throw new NotFoundException('Empresa não encontrada.');
+    }
+
+    const partner = this.partnerRepository.create({
+      ...createPartnerDto,
+    });
+
+    return this.partnerRepository.save(partner);
   }
 
-  findAll() {
-    return `This action returns all partners`;
+  async findAll(): Promise<Partner[]> {
+    return this.partnerRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} partner`;
+  async findOne(id: number): Promise<Partner> {
+    const partner = await this.partnerRepository.findOne({ where: { id } });
+    if (!partner) {
+      throw new NotFoundException('Sócio não encontrado.');
+    }
+    return partner;
   }
 
-  update(id: number, updatePartnerDto: UpdatePartnerDto) {
-    return `This action updates a #${id} partner`;
+  async update(id: number, updatePartnerDto: UpdatePartnerDto): Promise<Partner> {
+    const partner = await this.partnerRepository.findOne({ where: { id } });
+    if (!partner) {
+      throw new NotFoundException('Sócio não encontrado');
+    }
+    this.partnerRepository.merge(partner, updatePartnerDto);
+    return this.partnerRepository.save(partner);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} partner`;
+  async softDelete(id: number): Promise<void> {
+    const result = await this.partnerRepository.softDelete({ id });
+    if (result.affected === 0) {
+      throw new NotFoundException('Sócio não encontrado');
+    }
   }
 }
